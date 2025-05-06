6108846818110bfbcd784006444add707961dc7f
omarissasy <omar.issa.sy@gmail.com>
2025-05-06 11:14:32 +0200

    Deleted the old unnecessary dependencies and added Jackson package to both module and pom.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,18 +4,19 @@ module org.example {
     requires java.desktop;
     requires java.net.http;
     requires org.json;
-    requires spring.webflux;
-    requires spring.web;
-    requires reactor.core;
+//    requires spring.webflux;
+//    requires spring.web;
+//    requires reactor.core;
     requires com.fasterxml.jackson.datatype.jsr310;
     requires com.fasterxml.jackson.databind;
+    requires transitive javafx.graphics;
 
-    opens org.example to javafx.fxml;
+    exports org.example.model.dto to com.fasterxml.jackson.databind;
+    exports org.example.model.service to com.fasterxml.jackson.databind;
+    exports org.example.infrastructure to com.fasterxml.jackson.databind;
 
-    exports org.example.api;
-    opens org.example.api to javafx.fxml;
-    exports org.example.demo;
-    opens org.example.demo to javafx.fxml;
-    exports org.example.ui;
-    opens org.example.ui to javafx.fxml;
+//    opens org.example to javafx.fxml;
+//    exports org.example to javafx.graphics;
+//    exports org.example.api;
+//    opens org.example.api to javafx.fxml;
 }
