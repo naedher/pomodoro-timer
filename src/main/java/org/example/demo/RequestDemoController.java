d8818427b0faba974931c0b322b9d0d06fcdf4d2
naedher <166296413+naedher@users.noreply.github.com>
2025-05-07 22:27:47 +0200

    Add controller method for async example
    Also logging and comments

--- src/main/java/org/example/demo/RequestDemoController.java
+++ src/main/java/org/example/demo/RequestDemoController.java
@@ -4,18 +4,13 @@ import javafx.application.Platform;
 import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
-import javafx.stage.Stage;
 import org.example.demo.exception.BadRequestException;
 import org.example.demo.exception.UnauthorizedException;
 
-import java.io.IOException;
+import java.util.logging.Logger;
 
 public class RequestDemoController {
 
@@ -23,13 +18,21 @@ public class RequestDemoController {
     @FXML private TextField emailField;
     @FXML private TextField passwordField;
     @FXML private Text logText;
+    @FXML private TextField selectedTimer;
+    @FXML private Text timerText;
+    @FXML private Button getTimerButton;
+
+    private Logger logger;
 
     RequestDemoService service;
 
     public RequestDemoController() {
         this.service = new RequestDemoService();
+        this.logger = Logger.getLogger(RequestDemoController.class.getName());
     }
 
+
+    // Example of synchronous http requests with threads running concurrently
     @FXML
     public void login(ActionEvent event) {
 
@@ -42,12 +45,16 @@ public class RequestDemoController {
             @Override
             protected Void call() {
                 try {
+                    // Send credentials to the service to attempt login
                     service.login(email, password);
                 } catch (BadRequestException | UnauthorizedException e) {
+                    // Handle expected exceptions
+                    logger.warning(e.getMessage());
                     failed();
                     return null;
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    // Unexpected exceptions
+                    logger.severe(e.getMessage());
                     logText.setText("Something went wrong");
                     failed();
                     return null;
@@ -55,18 +62,19 @@ public class RequestDemoController {
 
                 // If login successful
                 // Navigate to the next view
-                Platform.runLater(() -> {
-                    try {
-                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/pomodoro.fxml"));
-                        Parent root = loader.load();
-                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-                        stage.setScene(new Scene(root));
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        logText.setText("Failed to load main scene.");
-                    }
-                });
+//                Platform.runLater(() -> { // Communication between threads happen through Platform.runLater
+//                    try {
+//                        FXMLLoader loader = new FXMLLoader(getClass().getResource("/org/example/pomodoro.fxml"));
+//                        Parent root = loader.load();
+//                        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//                        stage.setScene(new Scene(root));
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        logText.setText("Failed to load main scene.");
+//                    }
+//                });
 
+                logText.setText("Login successful");
                 return null;
             }
 
@@ -78,5 +86,33 @@ public class RequestDemoController {
 
         // Run the task in a new thread
         new Thread(loginTask).start();
+    }
+
+    // Example of async http requests, no need to manually create threads
+    @FXML
+    public void getTimerAsync() {
+        int id = Integer.parseInt(selectedTimer.getText());
+
+        service.getTimerAsync(id)
+                .thenAccept(timerDetails -> {
+                    // Run this code if successful
+                    timerText.setText("Successfully got timer: " + timerDetails.getName());
+                })
+                .exceptionally(completionException -> {
+                    // Run this code if any exceptions were thrown
+                    Throwable ex = completionException.getCause();
+                    // Check for expected exceptions and handle them
+                    if (ex instanceof BadRequestException) {
+                        // Malformed request or failed validation
+                        logger.severe(ex.getMessage());
+                    } else if (ex instanceof UnauthorizedException) {
+                        // Invalid email or password
+                        logger.severe(ex.getMessage());
+                    } else {
+                        // Unexpected error
+                        logger.severe(ex.getMessage());
+                    }
+                    return null;
+                });
     }
 }
