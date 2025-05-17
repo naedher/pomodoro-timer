5532a8a55f457337ec36d29b800ecb2e2376f1e4
mucansu <mucansu@gmail.com>
2025-05-17 14:12:10 +0200

    First draft to InMemoryTimerService, added normal Crup functions for local data

--- src/main/java/org/example/model/service/impl/InMemoryTimerService.java
+++ src/main/java/org/example/model/service/impl/InMemoryTimerService.java
@@ -5,6 +5,8 @@ import org.example.model.dto.TimerDetails;
 import org.example.model.dto.TimerUpdate;
 import org.example.model.service.TimerService;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
@@ -17,27 +19,45 @@ public class InMemoryTimerService implements TimerService {
 
     @Override
     public CompletableFuture<TimerDetails> getTimerDetails(long id) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Void> createTimer(TimerCreate timer) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Void> updateTimer(long id, TimerUpdate request) {
-        return null;
-    }
-
-    @Override
-    public CompletableFuture<Void> deleteTimer(long id) {
-        return null;
+        return CompletableFuture.completedFuture(store.get(id));
     }
 
     @Override
     public CompletableFuture<List<TimerDetails>> getUserTimers() {
-        return null;
+        return CompletableFuture.completedFuture(new ArrayList<>(store.values()));
     }
 
+    @Override
+    public CompletableFuture<Void> createTimer(TimerCreate req) {
+        long id = seq.getAndIncrement();
+        TimerDetails dto = new TimerDetails(
+                id,
+                req.getName(),
+                LocalDateTime.now(),
+                req.getWorkDuration(),
+                req.getShortBreakDuration(),
+                req.getLongBreakDuration(),
+                req.getPomodoroCount()
+        );
+        store.put(id, dto);
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletableFuture<Void> updateTimer(long id, TimerUpdate req) {
+        TimerDetails t = store.get(id);
+        if (t == null) return CompletableFuture.completedFuture(null);
+        t.setName(req.getName());
+        t.setWorkDuration(req.getWorkDuration());
+        t.setShortBreakDuration(req.getShortBreakDuration());
+        t.setLongBreakDuration(req.getLongBreakDuration());
+        t.setPomodoroCount(req.getPomodoroCount());
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletableFuture<Void> deleteTimer(long id) {
+        store.remove(id);
+        return CompletableFuture.completedFuture(null);
+    }
 }
