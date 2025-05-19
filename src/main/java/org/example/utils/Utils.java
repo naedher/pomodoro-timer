6b7e4c998c4696af1899b5ed22d7ffed6cd67d27
naedher <166296413+naedher@users.noreply.github.com>
2025-05-19 22:02:43 +0200

    Refactor TimerController
    Separate the timer behaviour to TimerManager
    The gui logic is now in TimerGui
    The list logic is now in TimerListManager

--- src/main/java/org/example/utils/Utils.java
+++ src/main/java/org/example/utils/Utils.java
@@ -1,8 +1,26 @@
 package org.example.utils;
 
+import javafx.scene.control.Alert;
+
+import javax.sound.sampled.*;
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class Utils {
 
     public static int minutesToSeconds(int minutes) {
         return minutes * 60;
     }
+
+    public static void showAlert(String title, String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+
+
 }
