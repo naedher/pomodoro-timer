a9038be740b76274c6dcaa37f1c25ba8f73d9a6f
naedher <166296413+naedher@users.noreply.github.com>
2025-06-02 11:23:12 +0200

    Remove unused imports

--- src/main/java/org/example/utils/Utils.java
+++ src/main/java/org/example/utils/Utils.java
@@ -2,11 +2,6 @@ package org.example.utils;
 
 import javafx.scene.control.Alert;
 
-import javax.sound.sampled.*;
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 public class Utils {
 
     public static int minutesToSeconds(int minutes) {
@@ -20,7 +15,4 @@ public class Utils {
         alert.setContentText(message);
         alert.showAndWait();
     }
-
-
-
 }
